be7451d214cc80830b29852a5dbff35477fb871a
Gabrielle <gabriellesantisilva@gmail.com>
2025-04-17 22:43:51 -0300

    Acréscimo da imagem ao formulário

--- delivery/src/main/resources/static/js/product.js
+++ delivery/src/main/resources/static/js/product.js
@@ -6,14 +6,15 @@ document.querySelector("form").addEventListener("submit", async function (e) {
       valueProduct: parseFloat(valueProduct.value),
       describe: describe.value
     };
-  
+    
     try {
       const response = await fetch("/admin/add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(productData)
       });
-  
+
+      uploadFiles();
       if (response.ok) {
         alert("Produto cadastrado com sucesso!!");
       } else {
@@ -22,5 +23,33 @@ document.querySelector("form").addEventListener("submit", async function (e) {
     } catch (error) {
       console.error("Erro:", error);
     }
-  });
+
+    async function uploadFiles() {
+      const file = document.getElementById("file");
+      const img = file.files[0];
+    
+      if (!img) {
+        alert("Selecione uma imagem antes de enviar.");
+        return;
+      }
+    
+      const formData = new FormData();
+      formData.append("file", img);
+    
+      try {
+        const response = await fetch("/api/upload", {
+          method: "POST",
+          body: formData
+        });
+    
+        if (response.ok) {
+          alert("Imagem enviada com sucesso!");
+        } else {
+          alert("Erro ao enviar imagem, tente novamente.");
+        }
+      } catch (error) {
+        console.error("Error Image:", error);
+      }
+    }    
+});
   
